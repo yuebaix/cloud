4c343220742b048317c6995f5fb546e181749d0b
JerryNiu <huifumanlove@hotmail.com>
2017-09-27 16:47:19 +0800

    [refactor](adjust module role) move consumer's hystrixboard to gateway

--- consumer/src/main/java/com/jufan/cloud/consumer/CloudConsumerApp.java
+++ consumer/src/main/java/com/jufan/cloud/consumer/CloudConsumerApp.java
@@ -1,26 +1,14 @@
 package com.jufan.cloud.consumer;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@EnableEurekaClient
-@EnableCircuitBreaker
-@EnableHystrixDashboard
+@SpringCloudApplication
 @EnableFeignClients("com.jufan.cloud.facade")
 @ComponentScan({"com.jufan.cloud.consumer", "com.jufan.cloud.facade.fallback"})
 public class CloudConsumerApp {
-//	@Bean
-//	@LoadBalanced
-//	RestTemplate restTemplate() {
-//		return new RestTemplate();
-//	}
-
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(CloudConsumerApp.class).web(true).run(args);
 	}
--- gateway/src/main/java/com/jufan/cloud/gateway/CloudGatewayApp.java
+++ gateway/src/main/java/com/jufan/cloud/gateway/CloudGatewayApp.java
@@ -2,10 +2,12 @@ package com.jufan.cloud.gateway;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringCloudApplication
 @EnableZuulProxy
+@EnableHystrixDashboard
 public class CloudGatewayApp {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(CloudGatewayApp.class).web(true).run(args);
