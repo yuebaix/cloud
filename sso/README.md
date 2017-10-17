e145abbe8cc4e8b1a6be883b11bceba23d18efde
JerryNiu <huifumanlove@hotmail.com>
2017-10-17 12:07:01 +0800

    add some test code

--- sso/README.md
+++ sso/README.md
@@ -1,6 +1,3 @@
-    授权url
-    localhost:8080/oauth/authorize?client_id=client&response_type=code&redirect_uri=http://www.baidu.com
-    
     获取token
     http://localhost:20000/sso/oauth/token?grant_type=password&username=mocker&password=nevergiveupnevergivein
     http://localhost:20000/sso/oauth/token
@@ -17,4 +14,5 @@
     测试获取用户信息
     http://localhost:20000/sso/user?access_token=92d21ffb-0e20-4dce-92b8-47f04ef43fa9
     
-    
+    测试权限控制
+    http://localhost:20000/sso/demo?access_token=76e9aea0-6514-41d2-b964-4b4c79dac675
--- sso/src/main/java/com/jufan/cloud/sso/controller/UserController.java
+++ sso/src/main/java/com/jufan/cloud/sso/controller/UserController.java
@@ -3,6 +3,7 @@ package com.jufan.cloud.sso.controller;
 import com.jufan.cloud.sso.domain.SysUser;
 import com.jufan.cloud.sso.repository.SysUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class UserController {
     public SysUser getUserDetails(){
         SysUser sysUser = sysUserRepository.findOne(1l);
         return sysUser;
+    }
+    //test authority
+    @GetMapping("/demo")
+    @PreAuthorize("hasAuthority('test')")
+    public String getDemo(){
+        return "good";
     }
 }
